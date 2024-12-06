551d25f1b91124f180a408a68cda4ecb0d5658b3
lawrenceorji <smarterbloke@gmail.com>
2024-12-06 15:00:39 +0100

    updated with trailer link

--- src/Movies.js
+++ src/Movies.js
@@ -61,7 +61,8 @@ const Movies = [
     rating: 7.9,
     imgURL:
       "https://m.media-amazon.com/images/M/MV5BMjFhZTcxOTktMzllMS00MzIzLWJhODEtZDU5YTFkNzRjZWQyXkEyXkFqcGc@._V1_QL75_UX100_CR0,5,100,148_.jpg",
-    trailerLink: "https://www.youtube.com/embed/vn-kVgVg6dw", // The Black Phone trailer link
+    trailerLink:
+      "https://www.youtube.com/embed/3eGP6im8AZA?si=ZZA-g0WM8uJIYQoX", // The Black Phone trailer link
   },
   {
     id: 7,
@@ -71,7 +72,8 @@ const Movies = [
     rating: 7.1,
     imgURL:
       "https://m.media-amazon.com/images/M/MV5BYjU1ZWMxYTUtNzQ1ZC00ZTcxLTg0NTMtMzY1ZmQyZjhmYjMyXkEyXkFqcGc@._V1_QL75_UX100_CR0,5,100,148_.jpg",
-    trailerLink: "https://www.youtube.com/embed/y6ZJ7XZ5rbE", // M3GAN trailer link
+    trailerLink:
+      "https://www.youtube.com/embed/BRb4U99OU80?si=QRumJvXJkWdPu5k_", // M3GAN trailer link
   },
   {
     id: 8,
@@ -81,7 +83,8 @@ const Movies = [
     rating: 7.5,
     imgURL:
       "https://m.media-amazon.com/images/M/MV5BMjM1ZmViMmYtOGYzZC00YzhmLWE0MTMtMzNjYzcyNjEwYWRkXkEyXkFqcGc@._V1_QL75_UX100_CR0,0,100,148_.jpg",
-    trailerLink: "https://www.youtube.com/embed/TV1vdtP5V2g", // Evil Dead Rise trailer link
+    trailerLink:
+      "https://www.youtube.com/embed/smTK_AeAPHs?si=NOXacB34i6AV22J4", // Evil Dead Rise trailer link
   },
   {
     id: 9,
@@ -91,7 +94,8 @@ const Movies = [
     rating: 8.2,
     imgURL:
       "https://m.media-amazon.com/images/M/MV5BNTc0YmQxMjEtODI5MC00NjFiLTlkMWUtOGQ5NjFmYWUyZGJhXkEyXkFqcGc@._V1_QL75_UX100_CR0,0,100,148_.jpg",
-    trailerLink: "https://www.youtube.com/embed/dpZF1kxovgA", // Dune: Part Two trailer link
+    trailerLink:
+      "https://www.youtube.com/embed/Way9Dexny3w?si=VBhOn2H0nSNdhqTD", // Dune: Part Two trailer link
   },
   {
     id: 10,
@@ -101,7 +105,8 @@ const Movies = [
     rating: 8.1,
     imgURL:
       "https://m.media-amazon.com/images/M/MV5BN2JkMDc5MGQtZjg3YS00NmFiLWIyZmQtZTJmNTM5MjVmYTQ4XkEyXkFqcGc@._V1_QL75_UX100_CR0,0,100,148_.jpg",
-    trailerLink: "https://www.youtube.com/embed/JJt8FzF0gF0", // Oppenheimer trailer link
+    trailerLink:
+      "https://www.youtube.com/embed/uYPbbksJxIg?si=EJw6QwzRDV1o-1jE", // Oppenheimer trailer link
   },
 ];
 
